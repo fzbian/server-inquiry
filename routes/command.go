344bbd1ff9c21777d412ac5b47bf723ffeb0481c
Fabian Martin <hello@fzbian.me>
2023-01-21 23:05:57 -0500

    refactor: reuse functions
    the token is read in a better way

--- routes/command.go
+++ routes/command.go
@@ -3,8 +3,6 @@ package routes
 import (
 	"github.com/fzbian/server-inquiry/utils"
 	"github.com/gofiber/fiber/v2"
-	"os/exec"
-	"sync"
 )
 
 func Command(c *fiber.Ctx) error {
@@ -21,31 +19,12 @@ func Command(c *fiber.Ctx) error {
 	} else {
 		command := c.FormValue("cmd")
 		so := c.FormValue("so")
-		var wg sync.WaitGroup
 
 		if so == "windows" {
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				cmd := exec.Command("powershell", command)
-				out, _ := cmd.Output()
-				err := c.SendString(string(out))
-				if err != nil {
-					return
-				}
-			}()
-			wg.Wait()
+			utils.ExecWindows(c, command)
 			return nil
 		} else if so == "linux" {
-			cmd := exec.Command(command)
-			out, err := cmd.Output()
-			if err != nil {
-				return err
-			}
-			err = c.SendString(string(out))
-			if err != nil {
-				return err
-			}
+			utils.ExecLinux(c, command)
 			return nil
 		}
 	}
