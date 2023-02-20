32b704474dcb06aaa7adca0d47c628d48467047d
Fabian Martin <hello@fzbian.me>
2023-02-19 19:37:49 -0500

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,10 @@ import (
 
 func main() {
 
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		AppName:               "Server Inquiry",
+		DisableStartupMessage: true,
+	})
 
 	api := app.Group("/api")
 
